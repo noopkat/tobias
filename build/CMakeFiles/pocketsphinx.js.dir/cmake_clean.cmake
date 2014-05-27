b7749cd0c47d4b9bb4b32e5f7892c227ad598356
Suz Hinton <suz.hinton@gmail.com>
2014-05-27 15:19:36 -0400

    removing c lib, etc, do not need it to run tobias

--- build/CMakeFiles/pocketsphinx.js.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "/Volumes/Macintosh HD/Volumes/Macintosh HD/Fidelius/sites/active/tobias/pocketsphinx.js/build/CMakeFiles/pocketsphinx.js"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/pocketsphinx.js.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/include/pocketsphinxjs-config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _POCKETSPHINXJS_CONFIG_H_
-#define _POCKETSPHINXJS_CONFIG_H_
-
-#define HMM_FOLDERS "rm1_200"
-
-/* #undef DICT_FILES */
-/* #undef LM_FILES */
-
-#endif /* _POCKETSPHINXJS_CONFIG_H_ */
